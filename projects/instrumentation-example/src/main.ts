13c5a9a7d855eb761e1f506759abfe420318493e
Tobias Staudt <tobias.staudt@vector.com>
2026-02-25 12:35:22 +0100

    chore(update-angular): migrate to angular v21

--- projects/instrumentation-example/src/main.ts
+++ projects/instrumentation-example/src/main.ts
@@ -1,4 +1,4 @@
-import { enableProdMode } from '@angular/core';
+import { enableProdMode, provideZoneChangeDetection } from '@angular/core';
 import { platformBrowserDynamic } from '@angular/platform-browser-dynamic';
 
 import { AppModule } from './app/app.module';
@@ -13,6 +13,6 @@ if (environment.production) {
 
   environment.openTelemetryConfig = config;
 
-  platformBrowserDynamic().bootstrapModule(AppModule)
+  platformBrowserDynamic().bootstrapModule(AppModule, { applicationProviders: [provideZoneChangeDetection()], })
     .catch(err => console.error(err));
 })();
